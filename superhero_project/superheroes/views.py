5e02c0a3c2af228dba8c585c03996d8a1e36d383
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-21 16:43:04 -0500

    added the details page about each individual hero, as well as made the html file for that

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Superhero
 
 # Create your views here.
@@ -10,3 +9,10 @@ def index(request):
         'all_heroes': all_heroes
     }
     return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheroes/details.html', context)
