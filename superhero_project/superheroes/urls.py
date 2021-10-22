a9318cce1968358ed2033a0e74ad430bf701d7e7
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-22 11:15:41 -0500

    added some a method to views to change details on the heros, as well as made a change.html file for that, i also added a url path

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name="detail"),
     path('new/', views.create, name='create'),
+    path('change/<int:hero_id>', views.change, name='change'),
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -26,7 +26,25 @@ def create(request):
         secondary = request.POST.get('secondary')
         catchphrase = request.POST.get('catchphrase')
         new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+
         new_hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def change(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+       'single_hero': single_hero
+    } 
+    if request.method == 'POST':
+        change_name = request.POST.get('change_name')
+        change_alter_ego = request.POST.get('change_alter_ego')
+        change_primary = request.POST.get('change_primary_ability')
+        change_secondary = request.POST.get('change_secondary_ability')
+        change_catchphrase = request.POST.get('change_catchphrase')
+        hero_id = Superhero(name=change_name, alter_ego=change_alter_ego, primary=change_primary, secondary=change_secondary, catchphrase=change_catchphrase)
+        hero_id.save()
+        return HttpResponseRedirect(reverse('superheroes:detail', context))
+    else:
+        return render(request, 'superheroes/change.html', context)
